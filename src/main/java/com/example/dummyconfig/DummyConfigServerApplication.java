d0ef24d31433e33e6a3a8503c40802598a302773
el.joju <mabm.dev@gmail.com>
2026-07-07 16:29:35 -0600

    se inicia proyecto

---
+++ src/main/java/com/example/dummyconfig/DummyConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.dummyconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class DummyConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dummyconfig/DummyConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dummyconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
